c9d4869ed8c9dbc063539feb61622070e35aea2f
taako-502 <i.am.armorknight@gmail.com>
2021-11-03 14:31:00 +0900

    ファーストビュー追加

--- functions.php
+++ functions.php
@@ -12,8 +12,8 @@ define( 'HEPERE_CHILD_THEME_URI', get_stylesheet_directory_uri() );
 add_action( 'wp_enqueue_scripts', function() {
   //CSS
   $dependency_style = array('font-awesome' , 'slick-theme', 'slick','hepere-app','hepere-style');
-  wp_enqueue_style( 'hepere-child-app', HEPERE_CHILD_THEME_URI.'/style/app.css', $dependency_style, "", true);
-  wp_enqueue_style( 'hepere-child-front', HEPERE_CHILD_THEME_URI.'/style/front.css', $dependency_style, "", true);
+  wp_enqueue_style( 'hepere-child-app', HEPERE_CHILD_THEME_URI.'/style/app.css', $dependency_style);
+  wp_enqueue_style( 'hepere-child-front', HEPERE_CHILD_THEME_URI.'/style/front.css', $dependency_style);
   wp_enqueue_style( 'hepere-child-style', HEPERE_CHILD_THEME_URI . '/style.css', $dependency_style);
   //JavaScript
   $dependency_script = array('jquery','hepere-main');
@@ -24,7 +24,21 @@ add_action( 'wp_enqueue_scripts', function() {
  * エディタ側にCSSをエンキュー
  */
 add_action( 'enqueue_block_editor_assets', function() {
-  wp_enqueue_style( 'hepere-child-editor', HEPERE_CHILD_THEME_URI.'/style/editor.css', array(), "", true);
-  wp_enqueue_style( 'hepere-child-front', HEPERE_CHILD_THEME_URI.'/style/app.css', array(), "", true);
-  wp_enqueue_style( 'hepere-child-style', HEPERE_CHILD_THEME_URI.'/style.css', array(), "", true);
+  wp_enqueue_style( 'hepere-child-editor', HEPERE_CHILD_THEME_URI.'/style/editor.css', array());
+  wp_enqueue_style( 'hepere-child-front', HEPERE_CHILD_THEME_URI.'/style/app.css', array());
+  wp_enqueue_style( 'hepere-child-style', HEPERE_CHILD_THEME_URI.'/style.css', array());
+});
+
+/**
+ * トップページのヘッダー下に挿入
+ */
+add_action( 'hepere_home_under_header', function() {
+  ?>
+  <div class="p-fv">
+    <p class="p-fv__phrase">
+      HEPEREは初めての<br>ブログ運営・個人ビジネスに最適な<br>WordPressテーマです
+    </p>
+    <div class="p-fv__box"></div>
+  </div>
+  <?php
 });
