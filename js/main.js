eb42de0b59f8278c112f5b1f36c6f31dd1e6e2d7
Claus123 <lucasclaus123@gmail.com>
2022-04-11 16:30:26 +0900

    機能紹介部分のjs修正、および各種スタイルの調整

--- js/main.js
+++ js/main.js
@@ -30,56 +30,169 @@
 
   });
 
+
   //「詳しく」ボタンをクリックした際に詳細内容を表示し、「詳しく」ボタンを非表示にさせ、「閉じる」ボタンを表示させる
   jQuery(document).ready(function() {
 
     jQuery('.detail_button_open').click(function() {
 
-    //「詳しく」ボタンを非表示にさせる
-    jQuery(this).toggle();
+      //「詳しく」ボタンを非表示にさせる
+      jQuery(this).toggle('slow');
 
-    //「閉じる」ボタンを表示させる
-    jQuery(this).parent().prev().children().toggle();
+      //「閉じる」ボタンを表示させる
+      jQuery(this).parent().prev().children().toggle('slow');
 
-    //詳細内容を開く
-    jQuery(this).parent().parent().next('.wp-block-columns').toggle();
+      //詳細内容を開く
+      jQuery(this).parent().parent().next('.wp-block-columns').toggle('slow');
 
-    //赤い帯部分の長さを調整
-    let functionsHeight =  jQuery('.functions').height();
-    
-    jQuery('.bg-red-left').css('height', functionsHeight);
-    
-    jQuery('.bg-red-right').css('height', functionsHeight);
-    });
+      //赤い帯部分の長さを調整
+        let functionsHeight = jQuery('.functions').height() - 300;
 
-  });  
+        let functionsPadding = Number(jQuery('#features-boxes-sp').css('padding-bottom').replace('px', ''));
+
+        let functionsMargin = Number(jQuery('.bg-red-left').css('margin-top').replace('px', ''));
+
+        let minusMarginPC = Number(jQuery('#features-boxes-pc').css('margin-top').replace('px', ''));
+
+        let minusMarginMD = Number(jQuery('#features-boxes-md').css('margin-top').replace('px', ''));
+
+        let minusMarginSP = Number(jQuery('#features-boxes-sp').css('margin-top').replace('px', ''));
+
+        let greyHeightPC = Number(jQuery('.bg-grey-pc').css('height').replace('px', ''));
+
+        let greyHeightMD = Number(jQuery('.bg-grey-md').css('height').replace('px', ''));
+
+        let greyHeightSP = Number(jQuery('.bg-grey-sp').css('height').replace('px', ''));
+
+        
+
+      jQuery('.bg-red-left').css('height', functionsHeight + 150);
+
+      jQuery('.bg-red-left').css('margin-top', functionsMargin - 450);
+      
+      jQuery('.bg-red-right').css('height', functionsHeight + 150);
+
+      jQuery('.bg-red-right').css('margin-top', functionsMargin - 450);
+
+
+      jQuery('.bg-red-left-sp').css('height', functionsHeight + 150);
+
+      jQuery('.bg-red-left-sp').css('margin-top', functionsMargin - 450);
+      
+      jQuery('.bg-red-right-sp').css('height', functionsHeight + 150);
+
+      jQuery('.bg-red-right-sp').css('margin-top', functionsMargin - 450);
+
+      jQuery('.bg-grey-pc').css('height', greyHeightPC + 250);
+
+      jQuery('.bg-grey-md').css('height', greyHeightMD + 300);
+
+      jQuery('.bg-grey-sp').css('height', greyHeightSP + 475);
+
+      jQuery('#features-boxes-pc').css('margin-top', minusMarginPC - 250);
+
+      jQuery('#features-boxes-md').css('margin-top', minusMarginMD - 300);
+
+      jQuery('#features-boxes-sp').css('margin-top', minusMarginSP - 475);
+
+
+
+  });
+
 
 //「閉じる」ボタンをクリックした際に詳細内容を非表示にし、「詳しく」ボタンを表示させ、「閉じる」ボタンを非表示にする
   jQuery(document).ready(function() {
 
     jQuery('.detail_button_close').click(function() {
     
-    //「閉じる」ボタンを非表示にさせる
-    jQuery(this).toggle();
-    
-    //「詳しく」ボタンを表示させる
-    jQuery(this).parent().next().children().toggle();
-    
-    //詳細内容を開く
-    jQuery(this).parent().parent().next('.wp-block-columns').toggle();
-    
-    //赤い帯部分の長さを調整
-    let functionsHeight =  jQuery('.functions').height();
-    jQuery('.bg-red-left').css('height', functionsHeight);
-    jQuery('.bg-red-right').css('height', functionsHeight);
+      //「閉じる」ボタンを非表示にさせる
+      jQuery(this).toggle('slow');
+      
+      //「詳しく」ボタンを表示させる
+      jQuery(this).parent().next().children().toggle('slow');
+      
+      //詳細内容を開く
+      jQuery(this).parent().parent().next('.wp-block-columns').toggle('slow');
+      
+      //赤い帯部分の長さを調整
+
+      let functionsHeight = jQuery('.functions').height() - 300;
+
+      let functionsPadding = Number(jQuery('#features-boxes-sp').css('padding-bottom').replace('px', ''));
+
+      let functionsMargin = Number(jQuery('.bg-red-left').css('margin-top').replace('px', ''));
+
+      let minusMarginPC = Number(jQuery('#features-boxes-pc').css('margin-top').replace('px', ''));
+
+      let minusMarginMD = Number(jQuery('#features-boxes-md').css('margin-top').replace('px', ''));
+
+      let minusMarginSP = Number(jQuery('#features-boxes-sp').css('margin-top').replace('px', ''));
+
+      let greyHeightPC = Number(jQuery('.bg-grey-pc').css('height').replace('px', ''));
+
+      let greyHeightMD = Number(jQuery('.bg-grey-md').css('height').replace('px', ''));
+
+      let greyHeightSP = Number(jQuery('.bg-grey-sp').css('height').replace('px', ''));
+
+
+      jQuery('.bg-red-left').css('height', functionsHeight - 150);
+
+      jQuery('.bg-red-left').css('margin-top', functionsMargin + 450);
+      
+      jQuery('.bg-red-right').css('height', functionsHeight - 150);
+
+      jQuery('.bg-red-right').css('margin-top', functionsMargin + 450);
+
+
+      jQuery('.bg-red-left-sp').css('height', functionsHeight - 250);
+
+      jQuery('.bg-red-left-sp').css('margin-top', functionsMargin + 450);
+      
+      jQuery('.bg-red-right-sp').css('height', functionsHeight - 250);
+
+      jQuery('.bg-red-right-sp').css('margin-top', functionsMargin + 450);
+
+      jQuery('.functions').css('margin-top', functionsMargin - 450);
+
+      jQuery('.bg-grey-pc').css('height', functionsMargin - 450);
+
+      jQuery('.bg-grey-pc').css('height', greyHeightPC - 250);
+
+      jQuery('.bg-grey-md').css('height', greyHeightMD - 300);
+
+      jQuery('.bg-grey-sp').css('height', greyHeightSP - 475);
+
+
+      jQuery('#features-boxes-pc').css('margin-top', minusMarginPC + 250);
+
+      jQuery('#features-boxes-md').css('margin-top', minusMarginMD + 300);
+
+      jQuery('#features-boxes-sp').css('margin-top', minusMarginSP + 475);
+
     });
 
-  });
 
   //スライドショー部分の設定
   jQuery(document).ready(function() {
     
-    jQuery('.features-boxes-sp').slick({
+    jQuery('#features-boxes-sp').slick({
+    
+      responsive: [{
+       breakpoint: 900,
+       settings: {
+        variableWidth: true,
+        centerMode: true,
+        centerPadding: '100px',
+        initialSlide: 1,
+        dots: true,
+        arrow: true,
+        autoplay: true
+        }
+      }],
+    
+    });
+
+    jQuery('#features-boxes-md').slick({
     
       responsive: [{
        breakpoint: 900,
@@ -98,3 +211,5 @@
   });
 
 
+  });
+});
